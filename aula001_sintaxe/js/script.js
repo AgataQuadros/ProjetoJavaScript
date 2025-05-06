b63fcbf0e83499d5a95b9e00e6a7a699f12c552a
AgataQuadros <quadrosagata@gmail>
2025-05-06 20:53:58 -0300

    Sintaxe Básica e Variáveis

---
+++ aula001_sintaxe/js/script.js
@@ -0,0 +1,19 @@
+var x = 10;
+var x = 20;
+console.log(x);
+
+let y = 30;
+
+y = 40;
+console.log(y);
+
+const z = 50;
+console.log(z);
+
+{
+    var a = 1;
+    let b = 2;
+    const c = 3;
+}
+
+console.log(a);
