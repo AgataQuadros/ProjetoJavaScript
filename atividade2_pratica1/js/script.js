1966cbd04cccb478dad3195caa7a65fa016eb67e
AgataQuadros <quadrosagata@gmail>
2025-05-20 20:35:51 -0300

    Um CU e meio

---
+++ atividade2_pratica1/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const resultado = document.getElementById('resultado');
+    const subtrair = document.getElementById('subtrair')
+    const zerar = document.getElementById('zerar')
+
+
+    document.getElementById('somar').addEventListener('click', function(){
+        let i = 0;
+
+        resultado.textContent = ++i;
+    })
+});
